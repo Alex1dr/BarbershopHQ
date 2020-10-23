a05c548d95c73e33f772f7f8400fc44b0f55dca3
Alex1dr <59291541+Alex1dr@users.noreply.github.com>
2020-10-23 18:10:33 +0300

    keep values on post

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ get '/' do
 end
 
 get '/visit' do
+  @c = Client.new
   erb :visit
 end
 
@@ -35,10 +36,11 @@ end
 
 post '/visit' do
 
-  c = Client.new params[:client]
-  if c.save
+  @c = Client.new params[:client]
+  if @c.save
     erb "<h2>Спасибо, Вы записались!</h2>"
   else
-    erb "<h2>Ошибка!</h2>"
+    @error =@c.errors.full_messages.first
+    erb :visit
   end
 end
